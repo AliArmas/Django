4ebcb5645326315ac28e65c771b2df48bf2918c8
Jose Ali Mendoza <josealipoot@gmai.com>
2020-10-17 01:05:38 -0500

    add model user

---
+++ backend/user/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class User(models.Model):
+    id = models.AutoField(primary_key = True)
+    name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    email = models.EmailField(max_length = 50)
+
+        
