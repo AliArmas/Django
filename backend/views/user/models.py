00e5ae665042080537d5807b9b1ffcc0930418b8
Jose Ali Mendoza <josealipoot@gmai.com>
2020-10-17 01:18:50 -0500

    a

--- backend/views/user/models.py
+++ backend/views/user/models.py
@@ -6,5 +6,8 @@ class User(models.Model):
     name = models.CharField(max_length = 50)
     last_name = models.CharField(max_length = 50)
     email = models.EmailField(max_length = 50)
+    phone_number = models.CharField(max_length = 10)
+    status = models.BooleanField()
+
 
         
